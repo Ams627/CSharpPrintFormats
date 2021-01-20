0e9bc92d4887df94a238b399d9874c49b436b9be
Adrian Sims <adriansims62781@gmail.com>
2021-01-20 11:19:35 +0100

    Updates.

--- CSharpPrintFormats/Program.cs
+++ CSharpPrintFormats/Program.cs
@@ -13,16 +13,8 @@ namespace CSharpPrintFormats
     {
         private static void Main()
         {
-            var i = 9876123;
-            Console.WriteLine($"{i:n0}");
-
-            var d = 9876123.45;
-            Console.WriteLine($"{d:n}");
-
-            decimal money = 123456789.453M;
-            Console.WriteLine($"{money:n}");
-            Console.WriteLine($"{money:n0}");
-            Console.WriteLine($"{money:n5}");
+            var i = 123;
+            Console.WriteLine($"{i,10:D4}");
         }
     }
 }

2d1269dfba04f19f488168832f92297cb21d3570
Adrian Sims <adriansims62781@gmail.com>
2021-01-20 10:50:40 +0100

    Add project files.

---
+++ CSharpPrintFormats/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpPrintFormats
+{
+    class Program
+    {
+        private static void Main()
+        {
+            var i = 1234;
+            var j = 5576788;
+            Console.WriteLine($"{i:10}{j}");
+        }
+    }
+}
